779b02822247f423597d6a456ba2ac4305349f18
CaptMouse <mauricio.maranho@gmail.com>
2025-03-03 18:43:54 +0000

    added boot.dev results for reference

--- stats.py
+++ stats.py
@@ -37,7 +37,18 @@ def get_sorted_char(dict):
 
     return list
 
-
 def sort_on(list):
     return list[1]
-    
+
+'''
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+'''
